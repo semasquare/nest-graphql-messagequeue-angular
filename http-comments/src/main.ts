063d7d8c08e347594c0337905001a4afb11453ff
Legiew <sebweigel@gmail.com>
2019-05-05 08:49:13 +0200

    add logging of requests to http services

--- http-comments/src/main.ts
+++ http-comments/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { CommentModule } from './comment.module';
 
+export function logger(req, res, next) {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  next();
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(CommentModule);
+  app.use(logger);
   await app.listen(3002);
 }
 
--- http-users/src/main.ts
+++ http-users/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
 
+export function logger(req, res, next) {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+  next();
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+  app.use(logger);
   await app.listen(3001);
 }
 
